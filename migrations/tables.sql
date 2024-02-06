192020d2df29635fae0378fb4065de2c7e655454
goodhumored <goodhumored@vk.com>
2024-02-06 22:28:44 +0300

    title pages and templates tables in migration

--- migrations/tables.sql
+++ migrations/tables.sql
@@ -14,6 +14,20 @@ CREATE TABLE IF NOT EXISTS
     PRIMARY KEY (id),
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id)
   );
+CREATE TABLE IF NOT EXISTS
+  title_pages (
+    id UUID UNIQUE,
+    name VARCHAR(100),
+    filename VARCHAR(100),
+    PRIMARY KEY (id)
+  );
+CREATE TABLE IF NOT EXISTS
+  templates (
+    id UUID UNIQUE,
+    name VARCHAR(100),
+    filename VARCHAR(100),
+    PRIMARY KEY (id)
+  );
 
 CREATE TABLE IF NOT EXISTS
   user_documents (
@@ -22,8 +36,12 @@ CREATE TABLE IF NOT EXISTS
     name VARCHAR(100),
     pdf_file_name VARCHAR(100),
     md_file_name VARCHAR(100),
+    title_page_id UUID,
+    template_id UUID,
     created_at TIMESTAMP,
     updated_at TIMESTAMP,
     PRIMARY KEY (id),
-    CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES users (id)
-  )
+    CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES users (id),
+    CONSTRAINT fk_title_page FOREIGN KEY (title_page_id) REFERENCES title_pages (id),
+    CONSTRAINT fk_template FOREIGN KEY (template_id) REFERENCES templates (id)
+  );
