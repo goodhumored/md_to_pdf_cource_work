db974e7801e61bcfd845b4f052fff1925247eb97
goodhumored <goodhumored@vk.com>
2024-02-06 22:33:45 +0300

    md to pdf converter rewritten to merge pdfs

--- src/domain/md-to-pdf-converter.ts
+++ src/domain/md-to-pdf-converter.ts
@@ -1,16 +1,49 @@
 import { spawn } from "child_process";
+import { rename, unlink } from "fs/promises";
 import { singleton } from "tsyringe";
+import UserDocument from "./user-document/user-document";
 
 @singleton()
 export default class MDToPDFConverter {
-  public convert(sourceFile: string, outFile = "out.pdf"): Promise<string> {
-    return new Promise((resolve, reject) => {
-      const pandoc = spawn("pdf", [sourceFile]);
+  public convertUserDoc(userDocument: UserDocument) {
+    return this.convert(
+      userDocument.getLocalMdFilePath(),
+      userDocument.getLocalPdfFilePath(),
+      userDocument.getTitlePage()?.getFilename(),
+      userDocument.getLatexTemplate()?.getFilename()
+    );
+  }
+
+  async convert(sourceFile: string, outPath: string, titlePath = "", templatePath = ""): Promise<void> {
+    await this.pandoc(sourceFile, templatePath);
+    if (titlePath) await this.addTitle(`${outPath}`, `public/${titlePath}`);
+  }
+
+  private pandoc(sourceMd: string, templatePath?: string) {
+    const args = [sourceMd];
+    if (templatePath) args.push("--template", `public/${templatePath}`);
+    return new Promise<void>((resolve, reject) => {
+      console.log("pdf", args.join(" "));
+      const pandoc = spawn("pdf", args);
       pandoc.on("error", (err) => reject(err));
+      pandoc.stdout.on("data", (data: Buffer) => console.log(data.toString()));
       pandoc.on("close", (code) => {
-        if (code === 0) resolve(outFile);
+        if (code === 0) resolve();
         reject(new Error(`proccess exited with code ${code}`));
       });
     });
+  }
+
+  private addTitle(pdfPath: string, titlePath: string) {
+    const outPath = `${pdfPath}-1`;
+    return new Promise<void>((resolve, reject) => {
+      const proccess = spawn("pdfunite", [titlePath, pdfPath, outPath]);
+      proccess.on("error", (err) => reject(err));
+      proccess.stdout.on("data", (data: Buffer) => console.log(data.toString()));
+      proccess.on("close", (code) => {
+        if (code === 0) resolve();
+        reject(new Error(`proccess exited with code ${code}`));
+      });
+    }).then(() => rename(outPath, pdfPath));
   }
 }
